730604087d120ab1d591ed4d1d01441e04f5b569
Euler Amorim <euler.amorim@hotmail.com>
2019-10-13 19:57:40 -0300

    Ultimas Alteracoes
    Alterando rotas e arquivos do projeto

---
+++ js/app/app.js
@@ -0,0 +1,15 @@
+angular.module("meuModulo",['ngRoute'])
+
+.config(function($routeProvider){
+    $routeProvider
+    .when("/home",{
+        templateUrl: "templates/home.html",
+        controller: "indexController"
+    })
+    .when("/contato",{
+        templateUrl: "templates/contato.html",        
+        controller: "contatoController"
+    });
+
+    $routeProvider.otherwise({redirectTo:"/home"});
+});
